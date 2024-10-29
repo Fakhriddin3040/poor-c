e9c393836d46178462978878ae4b9d8edd40b1b9
Fakhriddin <fahriddin3040@gmail.com>
2024-10-29 08:16:50 +0500

    Some fixes on sort choice argorightm.
    Signed-off-by: Fakhriddin <fahriddin3040@gmail.com>

--- algorithms/choice_sort.c
+++ algorithms/choice_sort.c
@@ -2,22 +2,17 @@
 #include <stdbool.h>
 
 
-void choiceSort(int *arr, size_t size)
-{   
+void choiceSort(int *arr, size_t size) {
     int min, tmp;
 
-    for(int left = 0; left < size - 1; left++)
-    {
+    for(int left = 0; left < size - 1; left++) {
         min = left;
-        for(int right = left + 1; right < size; right++)
-            {
-                if(arr[right] < arr[min])
-                {
-                    min = right;
+        for(int right = left + 1; right < size; right++) {
+            if(arr[right] < arr[min]) {
+                min = right;
                 }
             }
-        if(arr[min] < arr[left])
-        {
+        if(arr[min] < arr[left]) {
             tmp = arr[left];
             arr[left] = arr[min];
             arr[min] = tmp;
@@ -25,16 +20,14 @@ void choiceSort(int *arr, size_t size)
     }
 }
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
     int arr[] = {0, -21, 4, -1, 10, 5, 3, 10, 123};
     int *arr_ptr = arr;
     size_t arr_size = sizeof(arr) / sizeof(*arr);
 
     choiceSort(arr, arr_size);
 
-    while(arr_ptr != arr + (arr_size - 1))
-    {
+    while(arr_ptr != arr + arr_size) {
         printf("%d\n", *arr_ptr);
         arr_ptr++;
     }
